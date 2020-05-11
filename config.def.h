5349643cc76b22a41f7463354f0cb5d8f7dc0a46
Yiqun Ding <yiqunding@zju.edu.cn>
2020-05-11 13:46:51 +0800

    apply dpms patch
    This patch interacts with the Display Power Management Signaling
    and automatically turns off the monitor after a configurable time.
    The monitor is reactivated by a keystroke or moving the mouse.

--- config.def.h
+++ config.def.h
@@ -24,3 +24,6 @@ static const int failonclear = 0;
 
 /* time in seconds to cancel lock with mouse movement */
 static const int timetocancel = 4;
+
+/* time in seconds before the monitor shuts down */
+static const int monitortime = 60;
